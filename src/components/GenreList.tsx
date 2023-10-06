1d428d41c104a0add9ec285e7ce3c2d7e349c69e
mostafa.gh.79a <mostafa.gh.79a@gmail.com>
2023-10-06 06:31:08 +0330

    Refactor : Extract A query object

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,24 +1,46 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react"
-import useGenre, { Genre } from "../hooks/useGenere"
-
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+} from "@chakra-ui/react";
+import useGenre, { Genre } from "../hooks/useGenere";
 
 interface TGenreListProps {
-    onSelectGenre : (genre: Genre)=> void
-    selectedGenre : Genre | null
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({onSelectGenre, selectedGenre} : TGenreListProps) => {
-    const {data , isLoading}=useGenre()
+const GenreList = ({ onSelectGenre, selectedGenre }: TGenreListProps) => {
+  const { data, isLoading } = useGenre();
 
-        if (isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
   return (
     <List>
-        {data.map(genre => <ListItem paddingY="8px"  key={genre.id}> 
-        <HStack><Image borderRadius="8px" width="60px" height="40px" src={genre.image_background}/>
-        <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} sx={{textOverflow:"hidden"}} onClick={()=>onSelectGenre(genre)} variant="link" fontSize="lg">{genre.name}</Button>
-        </HStack>
-        </ListItem >)}
+      {data.map((genre) => (
+        <ListItem paddingY="8px" key={genre.id}>
+          <HStack>
+            <Image
+              borderRadius="8px"
+              width="60px"
+              height="40px"
+              src={genre.image_background}
+            />
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              sx={{ textOverflow: "hidden" }}
+              onClick={() => onSelectGenre(genre)}
+              variant="link"
+              fontSize="lg"
+            >
+              {genre.name}
+            </Button>
+          </HStack>
+        </ListItem>
+      ))}
     </List>
-  )
-}
+  );
+};
 
-export default GenreList
+export default GenreList;
